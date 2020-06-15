06893b2c99493939ae1362779ee221497a3ac4c8
Raychengole97 <raychengole97@gmail.com>
2020-06-16 00:20:27 +0300

    add all the projects files to the repository

---
+++ Desktop/quotes/src/app/quotes-form/quotes-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Quotes } from "../quotes";
+
+@Component({
+  selector: "app-quotes-form",
+  templateUrl: "./quotes-form.component.html",
+  styleUrls: ["./quotes-form.component.css"]
+})
+export class QuotesFormComponent implements OnInit {
+  newQuote = new Quotes(0, 0, "", "", "", "", new Date());
+
+  @Output() addQuote = new EventEmitter<Quotes>();
+
+  submitForm() {
+    //storing the user data to the local storage
+    localStorage.setItem("quotesFromTheUser", JSON.stringify(this.newQuote));
+  }
+
+  ngOnInit() {}
+}
